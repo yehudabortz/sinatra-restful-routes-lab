f2805a417e4eaa77c0cc934ee36b4e405c5f9761
Yehuda Bortz <yehudabortz@icloud.com>
2021-01-01 11:32:39 -0600

    commit tree b64734e216451b50fa9935fc62ecee595d13ece7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
 
+  get '/recipes' do 
+    erb :index
+  end
+
+  get '/recipes/new' do 
+    erb :'recipes/new'
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/show'
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect "/recipes/#{@recipe.id}"
+  end
+  
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect '/recipes'
+  end
 end
